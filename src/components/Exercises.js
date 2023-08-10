1db100f0758b9277536c3029605a0056d65558ad
Shoneel <shoneelkumar@gmail.com>
2023-08-10 21:17:51 +1200

    Develop Fitness-App Components

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Pages } from "@mui/icons-material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from "../utils/fetchData"; // Import utility functions
+import ExerciseCard from "./ExerciseCard"; // Import a custom component 
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="46px"
+      >
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "107px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {currentExercises.map((exercise, idx) => (
+          <ExerciseCard key={idx} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
+};
 
-export default Exercises
+export default Exercises;
